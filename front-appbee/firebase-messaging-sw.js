41b62112db71bbeaa568ef2c56427a3456d395b9
DridiAsma <asma.dridi@esen.tn>
2022-09-05 21:10:59 +0100

    Auto-generated commit [ci skip]

---
+++ front-appbee/firebase-messaging-sw.js
@@ -0,0 +1,14 @@
+importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBuSMu0Y_drw1dq1fftYhvAeLLf5RQmBDU",
+    authDomain: "notifiangular.firebaseapp.com",
+    projectId: "notifiangular",
+    storageBucket: "notifiangular.appspot.com",
+    messagingSenderId: "973410492450",
+    appId: "1:973410492450:web:56c0e59e114ffdb34eaa7b",
+    measurementId: "G-VQLP6CG3LX"
+  };
+
+  const messaging = firebase.messaging();
